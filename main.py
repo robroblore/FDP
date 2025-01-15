b916861e53163d7f5bc70f7f9fc091b8580cd844
Horia Robert <74012633+robroblore@users.noreply.github.com>
2025-01-15 10:38:18 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+FDP - File Delivery Protocol
+"""
+
+import server
+import client
+
+if __name__ == "__main__":
+    local = None
+    print("FDP - File Delivery Protocol")
+    print("1. Host Server")
+    print("2. Connect to Server")
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        local = server.Server()
+        local.start()
+    elif choice == 2:
+        local = client.Client()
+    else:
+        print("Invalid choice")
+
+
---
+++ server.py
@@ -0,0 +1,42 @@
+import socket
+import threading
+
+# FORMAT = The format (encryption) of the message to be received
+DEFAULT_FORMAT = "utf-8"
+
+# HEADERLEN = Information about the message to be received (in this case,
+# the length of the message)
+DEFAULT_HEADERLEN = 64
+
+DEFAULT_PORT = 6969  # Default port number for the server
+
+
+class Server:
+    def __init__(self):
+        self.FORMAT = DEFAULT_FORMAT
+        self.HEADERLEN = DEFAULT_HEADERLEN
+        self.PORT = DEFAULT_PORT
+
+        self.SERVER_IP = socket.gethostbyname(socket.gethostname())
+        self.ADDR = (self.SERVER_IP, DEFAULT_PORT)
+
+        # Create a socket object (AF_INET = IPv4, SOCK_STREAM = TCP)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Bind the socket to the port 6969
+        self.server.bind(self.ADDR)
+
+    def handle_client(self, conn):
+        pass
+
+    def start(self):
+        # Listen for incoming connections
+        self.server.listen()
+        print(f"Server is listening on {self.SERVER_IP}:{DEFAULT_PORT}")
+        while True:
+            # Accept the connection
+            conn, addr = self.server.accept()
+            # Print the address of the connected client
+            print(f"Connection from {addr} has been established.")
+            # Start a new thread to handle the connection
+            thread = threading.Thread(target=self.handle_client, args=(conn,))
+            thread.start()
